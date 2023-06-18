60b92923c4e204e78e74b67a96944e7919e834d6
svan9 <ivan.selyakovich@gmail.com>
2023-06-18 15:50:31 +0300

    18.06.23:15.50

---
+++ main.js
@@ -0,0 +1,11 @@
+// $(".trash").attr("draggable", "true");
+
+// $(".trash").on("dragstart", function () {
+//     this.classList.add("drag");
+// });
+
+
+// $(".trash").on("dragend", function () {
+//     this.classList.remove("drag");
+// });
+$(".container").html(`<div class="tbtn"></div>`.repeat(104))
