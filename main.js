7f6bc0ed1536ddeee782f5860608306688781615
svan9 <ivan.selyakovich@gmail.com>
2023-06-18 16:08:55 +0300

    18.06.23:16.08

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-// $(".trash").attr("draggable", "true");
+$(".container").html(`<div class="tbtn"></div>`.repeat(104));
 
-// $(".trash").on("dragstart", function () {
-//     this.classList.add("drag");
-// });
+const randint = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const r = randint(0, 104);
 
-// $(".trash").on("dragend", function () {
-//     this.classList.remove("drag");
-// });
-$(".container").html(`<div class="tbtn"></div>`.repeat(104))
+$($(".container > *").get(r)).on("click", function () {
+    document.body.innerHTML =
+        `<video src="cote.webm" style="width: 100%" autoplay loop></video>`.repeat(
+            4
+        );
+});
